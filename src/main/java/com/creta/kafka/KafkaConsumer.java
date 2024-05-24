a8a6d5a2b6821661183dc894506404f9a155a0e8
enos <enoskratos@hotmail.com>
2024-05-23 21:38:56 -0300

    commit generico

--- src/main/java/com/creta/kafka/KafkaConsumer.java
+++ src/main/java/com/creta/kafka/KafkaConsumer.java
@@ -11,6 +11,7 @@ public class KafkaConsumer {
     @KafkaListener(topics = "enos", groupId = "group")
     public void flightEventConsumer(TaskStatus taskStatus) {
         log.info("Consumer consume Kafka message -> {}", taskStatus);
+        System.out.println("Configurar log que nao seja o SL4J porque ele eh perigoso");
     }
 }
 
